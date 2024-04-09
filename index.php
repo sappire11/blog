e969d5598143c6d4656517d73d724473c0147cda
sappire11 <aa264555555@gmail.com>
2024-04-09 22:47:04 +0800

    套用bootstrap

--- index.php
+++ index.php
@@ -5,18 +5,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>blog 部落格</title>
+    <title>Blog 部落格</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>
-    <nav class="nav">
-        <h1>Blog</h1>
-        <a class="active" href="./index.php">首頁</a>
-        <a href="./about.php">關於我</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Blog</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./index.php">首頁</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./about.php">關於我</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
+
     <div class="container">
-        <div class="article">
+        <div class="row mt-3">
             <?php
             $sql = "SELECT * FROM articles ORDER BY create_at DESC";
             $result = $conn->query($sql);
@@ -24,16 +40,20 @@
                 while ($row = $result->fetch_assoc()) {
                     $id = $row['id'];
                     $title = $row['title'];
-                    echo "<div class='article'>";
-                    echo " <h1><a href='./article.php?id=$id'>$title</a></h1>";
+                    echo "<div class='col-md-4'>";
+                    echo "<div class='card mb-3'>";
+                    echo "<div class='card-body'>";
+                    echo "<h5 class='card-title'><a href='./article.php?id=$id'>$title</a></h5>";
+                    echo "</div>";
+                    echo "</div>";
                     echo "</div>";
                 }
             }
             ?>
-
-
-
         </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
