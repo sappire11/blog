8395802b9af97d5c8abf3a12f7b8d93a69a77b79
sappire11 <aa264555555@gmail.com>
2024-04-12 13:58:14 +0800

    套用bootstrap

--- article.php
+++ article.php
@@ -18,23 +18,39 @@ $content = $row['content'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>blog 部落格</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Blog 部落格</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
 </head>
 
 <body>
-    <nav class="nav">
-        <h1>Blog</h1>
-        <a class="active" href="./index.php">首頁</a>
-        <a href="./about.php">關於我</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="#">Blog</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="./index.php">首頁</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./about.php">關於我</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
-    <div class="container">
+
+    <div class="container mt-3">
         <div class="single-article">
             <h1><?php echo $title; ?></h1>
-            <h2>分類:<?php echo $row['name']; ?></h2>
+            <h2>分類: <?php echo $row['name']; ?></h2>
             <p><?php echo $content; ?></p>
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
